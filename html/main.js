8347ddb42c32eed6f5589b24d259f9f7001a7305
NemethPal <73165570+NemethPal@users.noreply.github.com>
2021-09-25 19:34:32 +0200

    Add files via upload

---
+++ html/main.js
@@ -0,0 +1,17 @@
+function calcAmount() {
+
+   
+    let price = 1000;
+    let amount = document.querySelector("input[name='amount-number']");
+    let show = document.querySelector("span.show-amount");
+    if(amount.value > 10 || amount.value < 1) {
+        alert("nem jó!!!")
+        show.innerHTML = 0;
+        return;
+    }
+    show.innerHTML = parseInt(price * amount.value);
+
+
+
+   
+}
